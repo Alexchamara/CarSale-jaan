59b678aef1de09ddeffaad32096a4a009581118d
N.D.V.Ovinya Perera <windypereira39@gmail.com>
2026-03-17 12:41:33 +0530

    change port

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import os
 
-bind = os.getenv("GUNICORN_BIND", "127.0.0.1:8000")
+bind = os.getenv("GUNICORN_BIND", "127.0.0.1:8006")
 workers = int(os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
 threads = int(os.getenv("GUNICORN_THREADS", 2))
 timeout = int(os.getenv("GUNICORN_TIMEOUT", 60))
